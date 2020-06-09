cdc7be0dd57f7cdca940c7fa17d4ca90818f7a62
nijarhock <nijarixa@gmail.com>
2020-06-09 15:23:46 +0700

    First Commit

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>About Random User</h2>
+                <p>RANDOM USER GENERATOR A free, open-source API for generating random user data. Like Lorem Ipsum, but people.</p>
+            </div>
+        );
+    }
+}
+
+export default About;
